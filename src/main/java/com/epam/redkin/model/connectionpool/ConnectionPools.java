fbfd4350dfd0c1b319484a80c46ea92f169a4f50
denis <redkindo@gmail.com>
2023-01-17 01:48:47 +0200

    delete test hikari

--- src/main/java/com/epam/redkin/model/connectionpool/ConnectionPools.java
+++ src/main/java/com/epam/redkin/model/connectionpool/ConnectionPools.java
@@ -2,45 +2,21 @@ package com.epam.redkin.model.connectionpool;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import javax.sql.DataSource;
 
 public class ConnectionPools {
-    private static final Logger logger = LoggerFactory.getLogger(ConnectionPools.class);
-    private static final String PROPERTIES_FILE_TEST = "/testdb.properties";
     private static final String PROPERTIES_FILE = "/db.properties";
-    private static final HikariDataSource dataSourceTest;
     private static final HikariDataSource dataSource;
     static {
-        HikariConfig testConfig = new HikariConfig(PROPERTIES_FILE_TEST);
         HikariConfig config = new HikariConfig(PROPERTIES_FILE);
         config.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        testConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceTest = new HikariDataSource(testConfig);
         dataSource = new HikariDataSource(config);
-    }
-
-
-    private enum Testing {
-        INSTANCE(ConnectionPools.dataSourceTest);
-        private final HikariDataSource dataSource;
-        private Testing(HikariDataSource dataSource) {
-            this.dataSource = dataSource;
-        }
-        public HikariDataSource getDataSource() {
-            return dataSource;
-        }
-    }
-    public static HikariDataSource getTesting() {
-        return Testing.INSTANCE.getDataSource();
     }
 
     private enum Processing {
         INSTANCE(ConnectionPools.dataSource);
         private final HikariDataSource dataSource;
-        private Processing(HikariDataSource dataSource) {
+        Processing(HikariDataSource dataSource) {
             this.dataSource = dataSource;
         }
         public HikariDataSource getDataSource() {
