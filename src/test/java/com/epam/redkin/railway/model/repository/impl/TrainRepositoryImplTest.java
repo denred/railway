b8a38a8bbebb56be8653f03e1d68cecb53b3fd21
denis <redkindo@gmail.com>
2023-01-23 09:09:30 +0200

    add tests

--- src/test/java/com/epam/redkin/railway/model/repository/impl/TrainRepositoryImplTest.java
+++ src/test/java/com/epam/redkin/railway/model/repository/impl/TrainRepositoryImplTest.java
@@ -13,7 +13,6 @@ import javax.sql.DataSource;
 import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
